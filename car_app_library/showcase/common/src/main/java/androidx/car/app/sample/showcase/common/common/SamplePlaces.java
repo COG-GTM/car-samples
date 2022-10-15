7b13d69026e165a1a6bc4176b4fa2e958d988c8d
Jerry Chen <jech@google.com>
2022-10-15 00:30:50 +0000

    Revert "Internal change"
    This reverts commit 9a693d57a7fa6f95c15a8ffaa61a24f5920f84b2.
    
    Reason for revert: New changes requiring CarAppApiLevel 6 included in this CL, I have updated Github separately so we can keep revert this and wait for the next CAL release.
    
    Change-Id: I1dd93e35940d4cf7ac76d1d091714e27cdae44bc

--- car_app_library/showcase/common/src/main/java/androidx/car/app/sample/showcase/common/common/SamplePlaces.java
+++ car_app_library/showcase/common/src/main/java/androidx/car/app/sample/showcase/common/common/SamplePlaces.java
@@ -19,7 +19,6 @@ package androidx.car.app.sample.showcase.common.common;
 import static java.lang.Math.max;
 import static java.lang.Math.min;
 
-import android.content.res.TypedArray;
 import android.graphics.BitmapFactory;
 import android.location.Location;
 import android.text.SpannableString;
@@ -47,7 +46,6 @@ import androidx.core.graphics.drawable.IconCompat;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 /** Provides sample place data used in the demos. */
 public class SamplePlaces {
@@ -85,14 +83,6 @@ public class SamplePlaces {
     private static List<PlaceInfo> getSamplePlaces(@NonNull CarContext carContext) {
         List<PlaceInfo> places = new ArrayList<>();
 
-        TypedArray typedArray =
-                carContext.obtainStyledAttributes(R.style.CarAppTheme, R.styleable.ShowcaseTheme);
-        CarColor iconTintColor =
-                CarColor.createCustom(
-                        typedArray.getColor(R.styleable.ShowcaseTheme_markerIconTintColor, -1),
-                        typedArray.getColor(R.styleable.ShowcaseTheme_markerIconTintColorDark, -1));
-
-
         Location location1 = new Location(SamplePlaces.class.getSimpleName());
         location1.setLatitude(47.6696482);
         location1.setLongitude(-122.19950278);
@@ -109,7 +99,7 @@ public class SamplePlaces {
                                                 IconCompat.createWithResource(
                                                         carContext,
                                                         R.drawable.ic_commute_24px))
-                                                .setTint(iconTintColor)
+                                                .setTint(CarColor.BLUE)
                                                 .build(),
                                         PlaceMarker.TYPE_ICON)
                                 .build()));
@@ -252,7 +242,7 @@ public class SamplePlaces {
 
     /** Return the {@link ItemList} of the sample places. */
     @NonNull
-    public ItemList getPlaceList(boolean randomOrder) {
+    public ItemList getPlaceList() {
         ItemList.Builder listBuilder = new ItemList.Builder();
 
         int listLimit = 6;
@@ -268,7 +258,7 @@ public class SamplePlaces {
         listLimit = min(listLimit, mPlaces.size());
 
         for (int index = 0; index < listLimit; index++) {
-            PlaceInfo place = mPlaces.get(randomOrder ? new Random().nextInt(listLimit) : index);
+            PlaceInfo place = mPlaces.get(index);
 
             // Build a description string that includes the required distance span.
             int distanceKm = getDistanceFromCurrentLocation(place.location) / 1000;
